bfa9c060eb514996888774fea1f84e8b41948afb
Marvan Al Kufai <alkufai@gmail.com>
2024-06-18 18:09:50 +0200

    update(wezterm): disabled func split_nav

--- common/.config/wezterm/config/keybinds.lua
+++ common/.config/wezterm/config/keybinds.lua
@@ -5,42 +5,42 @@ local nvim_path = "/home/linuxbrew/.linuxbrew/bin/nvim"
 local Keybinds = {}
 
 -- Helper functions for keybinds
-local direction_keys = {
-	Left = "h",
-	Down = "j",
-	Up = "k",
-	Right = "l",
-	-- reverse lookup
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
-}
-local function is_vim(pane)
-	-- this is set by smart-splits.nvim Neovim plugin, and unset on ExitPre in Neovim
-	return pane:get_user_vars().IS_NVIM == "true"
-end
+-- local direction_keys = {
+-- 	Left = "h",
+-- 	Down = "j",
+-- 	Up = "k",
+-- 	Right = "l",
+-- 	-- reverse lookup
+-- 	h = "Left",
+-- 	j = "Down",
+-- 	k = "Up",
+-- 	l = "Right",
+-- }
+-- local function is_vim(pane)
+-- 	-- this is set by smart-splits.nvim Neovim plugin, and unset on ExitPre in Neovim
+-- 	return pane:get_user_vars().IS_NVIM == "true"
+-- end
 
-local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
-end
+-- local function split_nav(resize_or_move, key)
+-- 	return {
+-- 		key = key,
+-- 		mods = resize_or_move == "resize" and "META" or "CTRL",
+-- 		action = wezterm.action_callback(function(win, pane)
+-- 			if is_vim(pane) then
+-- 				-- pass the keys through to vim/nvim
+-- 				win:perform_action({
+-- 					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+-- 				}, pane)
+-- 			else
+-- 				if resize_or_move == "resize" then
+-- 					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+-- 				else
+-- 					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+-- 				end
+-- 			end
+-- 		end),
+-- 	}
+-- end
 
 function Keybinds.setup(config)
 	config.keys = {
@@ -175,7 +175,7 @@ function Keybinds.setup(config)
 				end),
 			}),
 		},
-		{ key = "PageUp",   mods = "SHIFT", action = act.ScrollByPage(-0.5) },
+		{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-0.5) },
 		{ key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(0.5) },
 	}
 	for i = 1, 9 do
