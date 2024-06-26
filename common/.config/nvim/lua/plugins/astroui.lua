692e61830b8f687856d4e665e1311dfe09458d8c
Marvan Al Kufai <alkufai@gmail.com>
2024-06-26 21:08:12 +0200

    update(nvim): add transparent.nvim and material.nvim

--- common/.config/nvim/lua/plugins/astroui.lua
+++ common/.config/nvim/lua/plugins/astroui.lua
@@ -3,10 +3,9 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "midnight",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    -- colorscheme = "midnight",
+    colorscheme = "material-deep-ocean",
     -- highlights = {
     --   init = { -- this table overrides highlights in all themes
     --     -- Normal = { bg = "#000000" },
@@ -14,20 +13,6 @@ return {
     --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
     --     -- Normal = { bg = "#000000" },
     --   },
-    -- },
-    -- -- Icons can be configured throughout the interface
-    -- icons = {
-    --   -- configure the loading of the lsp in the status line
-    --   LSPLoading1 = "⠋",
-    --   LSPLoading2 = "⠙",
-    --   LSPLoading3 = "⠹",
-    --   LSPLoading4 = "⠸",
-    --   LSPLoading5 = "⠼",
-    --   LSPLoading6 = "⠴",
-    --   LSPLoading7 = "⠦",
-    --   LSPLoading8 = "⠧",
-    --   LSPLoading9 = "⠇",
-    --   LSPLoading10 = "⠏",
     -- },
   },
 }
---
+++ common/.config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,8 @@
+return {
+  "xiyaowong/transparent.nvim",
+  opts = {
+    extra_groups = {
+      "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+    },
+  },
+}
