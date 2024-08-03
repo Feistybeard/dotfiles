fa95373f8777f10675333672aa795e8f451362e8
Marvan Al Kufai <alkufai@gmail.com>
2024-08-03 11:39:01 +0200

    update(nvim): disable catppuccin

--- common/.config/nvim/lua/plugins/disabled.lua
+++ common/.config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
